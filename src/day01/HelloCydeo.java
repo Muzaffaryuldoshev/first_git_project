d48dbc530e6ddab7879cdc656addc5566ab62fef
Muzaffar Yuldoshev <yuldoshevmuzaffarr@gmail.com>
2024-12-14 19:00:54 +0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,5 +4,9 @@ public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
         System.out.println("cOMMINT AND PUSH");
+                System.out.println("Hello Bro");
+
+                System.out.println("Hello Programmers");
+
     }
 }
